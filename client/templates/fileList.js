dcc0832900f08afd67244d191b52e8218c6c2ba1
Andrew Luo <andrewjluo@gmail.com>
2015-06-27 15:15:42 -0700

    changed a name

---
+++ client/templates/fileList.js
@@ -0,0 +1,12 @@
+if (Meteor.isClient) {
+  Template.fileList.helpers({
+    files: [
+      {title: "Happy face", description: "I am a happy face"},
+      {title: "Happy face", description: "I am a happy face"},
+      {title: "Happy face", description: "I am a happy face"},
+      {title: "Happy face", description: "I am a happy face"},
+      {title: "Happy face", description: "I am a happy face"},
+      {title: "Happy face", description: "I am a happy face"},
+    ]
+  });
+}
