514e782c13309917f051454e77a97fe9156b0b3c
Katherine Lee <kate.lee168@gmail.com>
2015-06-28 09:37:53 -0700

    more styling dropdown

--- client/templates/actionsBar.js
+++ client/templates/actionsBar.js
@@ -1,25 +1,31 @@
 Template.actionsBar.events({
   "click #upload": function () {
-     var e = document.getElementById("uploadForm");
-     console.log(e);
-     if(e.style.display == 'block')
-        e.style.display = 'none';
-     else
-        e.style.display = 'block';
-  },
+       var e = document.getElementById("uploadForm");
+       var e2 = document.getElementById("newDirForm");
+       console.log(e);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else {
+          e.style.display = 'block';
+          e2.style.display= 'none';
+        }
+    },
   "click #add": function () {
-    var e = document.getElementById("newDirForm");
-     console.log(e);
-     if(e.style.display == 'block')
-        e.style.display = 'none';
-     else
-        e.style.display = 'block';
-  },
+       var e = document.getElementById("newDirForm");
+       var e2 = document.getElementById("uploadForm");
+       console.log(e);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else {
+          e.style.display = 'block';
+          e2.style.display= 'none';
+        }
+    },
 
-  "click #download": function () {
-    console.log("clicked download")
-  },
-  "click #trash": function() {
-    console.log("clicked trash")
+  "click #left": function () {
+    var e = document.getElementById("uploadForm");
+    var e2 = document.getElementById("newDirForm");
+    e.style.display = 'none';
+    e2.style.display = 'none';
   }
 });
