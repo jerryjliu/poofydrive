c62945469fa75665a8484c5845da6940999ec90d
Evelyn Ding <evelynjding@gmail.com>
2015-06-27 19:11:49 -0700

    add google authentication

--- client/poofydrive.js
+++ client/poofydrive.js
@@ -12,6 +12,22 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+
+
+      // Allows us to access Google Drive by getting permission
+        Meteor.loginWithGoogle({
+          requestPermissions: [],
+          loginStyle: "popup"
+        }, function(err) {
+          if(err) {
+            console.log('Error: ', err);
+          } else {
+            Router.go('home');
+          }
+        })
+
+
+
     }
   });
 }
