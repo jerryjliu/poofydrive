d43aca9fed1f29214dd8910ea74e63f923aa02e1
Jerry Liu <jerryjliu98@gmail.com>
2015-06-27 22:22:36 -0700

    polished upload, download, delete

--- client/poofydrive.js
+++ client/poofydrive.js
@@ -2,6 +2,11 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  Meteor.startup(function() {
+     Meteor.call('processFile', 'test', '../../../../../test_dir/', [0.6, 0.4]);
+     Meteor.call('unprocessFile', 'test', '../../../../../test_dir/', 2);
+  });
+
   // Template.hello.helpers({
   //   counter: function () {
   //     return Session.get('counter');
