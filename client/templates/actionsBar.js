fc6cadc1e5eb81d640af345128e40171c3c9eebd
Katherine Lee <kate.lee168@gmail.com>
2015-06-28 09:13:38 -0700

    trying to revert back

--- client/templates/actionsBar.js
+++ client/templates/actionsBar.js
@@ -1,51 +1,19 @@
 Template.actionsBar.events({
-  /*"click #upload": function () {
-       var e = document.getElementById("uploadForm");
-       console.log(e);
-       if(e.style.display == 'block')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';
-    },
   "click #upload": function () {
-    $( "#upload" ).accordion();
-  },*/
-  "click .accordion-upload-title": function(e) {
-      // Grab current anchor value
-      console.log("upload");
-      var currentAttrValue = $(this).attr('href');
-
-      if($(e.target).is('.active')) {
-          $('.accordion .accordion-upload-title').removeClass('active');
-          $('.accordion .accordion-upload-content').slideUp(300).removeClass('open');
-      }else {
-          $('.accordion .accordion-upload-title').addClass('active');
-          $('.accordion .accordion-upload-content').slideDown(300).addClass('open');
-
-          // Add active class to upload title
-          $(this).addClass('active');
-          // Open up the hidden content panel
-          $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
-      }
-
-      e.preventDefault();
+     var e = document.getElementById("uploadForm");
+     console.log(e);
+     if(e.style.display == 'block')
+        e.style.display = 'none';
+     else
+        e.style.display = 'block';
   },
-  "click .accordion-directory-title": function() {
-      // Grab current anchor value
-      console.log("directory");
-      var e = document.getElementById("newDirForm");
-      var currentAttrValue = $(this).attr('href');
-      if(e.style.display == 'block') {
-          e.style.display = 'none';
-          $('.accordion .accordion-directory-content').slideUp(300).removeClass('open');
-      }
-      else {
-          e.style.display = 'block';
-          console.log($('.accordion .accordion-directory-title'));
-          $('.accordion .accordion-directory-content').slideDown(300).addClass('open');
-      }
-
-      e.preventDefault();
+  "click #add": function () {
+    var e = document.getElementById("newDirForm");
+     console.log(e);
+     if(e.style.display == 'block')
+        e.style.display = 'none';
+     else
+        e.style.display = 'block';
   },
 
   "click #download": function () {
--- client/templates/loggedIn.js
+++ client/templates/loggedIn.js
@@ -1,21 +1,3 @@
 Template.loggedIn.events({
-  "click .accordion-section-title": function(e) {
-      // Grab current anchor value
-      var currentAttrValue = $(this).attr('href');
-
-      if($(e.target).is('.active')) {
-          $('.accordion .accordion-section-title').removeClass('active');
-          $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
-      }else {
-          $('.accordion .accordion-section-title').removeClass('active');
-          $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
-
-          // Add active class to section title
-          $(this).addClass('active');
-          // Open up the hidden content panel
-          $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
-      }
-
-      e.preventDefault();
-  }
+  
 });
