2b5a3432a821d04e13b2ee1e960800045dc9ddaf
Andrew Luo <andrewjluo@gmail.com>
2015-06-27 23:09:03 -0700

    Moved some upload stuff around

--- client/poofydrive.js
+++ client/poofydrive.js
@@ -15,26 +15,26 @@ if (Meteor.isClient) {
   // });
 
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
 
 
-      // Allows us to access Google Drive by getting permission
-        Meteor.loginWithGoogle({
-          requestPermissions: [],
-          loginStyle: "popup"
-        }, function(err) {
-          if(err) {
-            console.log('Error: ', err);
-          } else {
-            Router.go('home');
-          }
-        })
+  //     // Allows us to access Google Drive by getting permission
+  //       Meteor.loginWithGoogle({
+  //         requestPermissions: [],
+  //         loginStyle: "popup"
+  //       }, function(err) {
+  //         if(err) {
+  //           console.log('Error: ', err);
+  //         } else {
+  //           Router.go('home');
+  //         }
+  //       })
 
 
 
-    }
-  });
+  //   }
+  // });
 }
